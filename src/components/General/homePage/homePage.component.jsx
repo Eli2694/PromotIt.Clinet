dc917af3db9a64c82bd2408b359618c60cf373bd
Eli2694 <111525174+Eli2694@users.noreply.github.com>
2023-01-16 14:01:05 +0200

    add contact

--- src/components/General/homePage/homePage.component.jsx
+++ src/components/General/homePage/homePage.component.jsx
@@ -18,18 +18,22 @@ export const HomePage = () => {
   const { user } = useAuth0();
   const navigate = useNavigate();
 
+  //Bring Campaign products by id and save them in variable from useContext
   const handleCampaignProducts = async (CampaignId) => {
     let campaignProducts = await getCProducts(CampaignId);
     setProductsList(campaignProducts);
+    //Change to page in the address "/usersProducts"
     navigate('/usersProducts');
   };
 
+  //If it is an Activist user he can go directly to campaign products and start buying with points
   const handleActivistPoints = async () => {
     let Email = user.email;
     let points = await getActivistPoints(Email);
     setPoints(points);
   };
 
+  //get all information about campaigns
   const FullInfoAboutCampaigns = async () => {
     let Campaigns = await getFullListOfCampaigns();
     setCampaigns(Campaigns);
@@ -41,50 +45,61 @@ export const HomePage = () => {
   }, []);
 
   return (
-    <main>
-      {' '}
-      <Table striped bordered hover className='table'>
-        <thead>
-          <tr>
-            <th>Association</th>
-            <th>Association Website</th>
-            <th>Campaign Name</th>
-            <th>Campaign Website</th>
-            <th>Campaign Hashtag</th>
-            <th>Donation amount</th>
-            <th>Campaign Products</th>
-          </tr>
-        </thead>
-        <tbody>
-          {campaigns &&
-            campaigns.map((info) => {
-              const {
-                CampaignId,
-                AssociationName,
-                AssociationWebsite,
-                campaignName,
-                campaignWebsite,
-                campaginHashtag,
-                donationAmount,
-              } = info;
-              const urlObject = new URL(campaignWebsite);
-              const urlObject2 = new URL(AssociationWebsite);
-              const cWebsite = urlObject.hostname;
-              const aWebsite = urlObject2.hostname;
-              return (
-                <HomePageRow
-                  AssociationName={AssociationName}
-                  AssociationWebsite={aWebsite}
-                  campaignName={campaignName}
-                  campaignWebsite={cWebsite}
-                  campaginHashtag={campaginHashtag}
-                  donationAmount={donationAmount}
-                  handleProductsList={() => handleCampaignProducts(CampaignId)}
-                ></HomePageRow>
-              );
-            })}
-        </tbody>
-      </Table>
-    </main>
+    <>
+      <main>
+        {' '}
+        <Table striped bordered hover className='table'>
+          <thead>
+            <tr>
+              <th>Association</th>
+              <th>Association Website</th>
+              <th>Campaign Name</th>
+              <th>Campaign Website</th>
+              <th>Campaign Hashtag</th>
+              <th>Donation amount</th>
+              <th>Campaign Products</th>
+            </tr>
+          </thead>
+          <tbody>
+            {campaigns &&
+              campaigns.map((info) => {
+                const {
+                  CampaignId,
+                  AssociationName,
+                  AssociationWebsite,
+                  campaignName,
+                  campaignWebsite,
+                  campaginHashtag,
+                  donationAmount,
+                } = info;
+                const urlObject = new URL(campaignWebsite);
+                const urlObject2 = new URL(AssociationWebsite);
+                const cWebsite = urlObject.hostname;
+                const aWebsite = urlObject2.hostname;
+                return (
+                  <HomePageRow
+                    AssociationName={AssociationName}
+                    AssociationWebsite={aWebsite}
+                    campaignName={campaignName}
+                    campaignWebsite={cWebsite}
+                    campaginHashtag={campaginHashtag}
+                    donationAmount={donationAmount}
+                    handleProductsList={() =>
+                      handleCampaignProducts(CampaignId)
+                    }
+                  ></HomePageRow>
+                );
+              })}
+          </tbody>
+        </Table>
+      </main>
+      <footer>
+        <div className='contact-container'>
+          <h3>Contact Information</h3>
+          <p>Email: ContactUs@gmail.com</p>
+          <p>Phone: *2023</p>
+        </div>
+      </footer>
+    </>
   );
 };
